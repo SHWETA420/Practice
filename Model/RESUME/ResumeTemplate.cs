db4c05794dee623d0fa1385aa23d7cec40164ba8
Shweta <101639796+SHWETA420@users.noreply.github.com>
2025-04-25 12:16:44 +0530

    small changes

--- Model/RESUME/ResumeTemplate.cs
+++ Model/RESUME/ResumeTemplate.cs
@@ -14,5 +14,6 @@ namespace Model.RESUME
         public Resume Template { get; set; }
         public string Category { get; set; }
         public string PreviewImagePath { get; set; } // Path to image in wwwroot
+        public string Preview { get; set; } // Path to image in wwwroot
     }
 }
